512dc6e9a095a9f7ffc48a9bef9a4483f0a018ea
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-06-22 17:31:46 +0530

    Add files via upload

---
+++ C to F.py
@@ -0,0 +1,6 @@
+print("Hi welcome to the Celius to farenheat coverter")
+#Here I printed some message
+C=int(input("Enter the the celius value: "))
+#Here I will take input from the user of c value
+print("The farenheat value:"(C*9/5)+32)
+#And I finally added the formula and printed the value
